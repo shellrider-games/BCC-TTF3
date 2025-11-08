5a52ba57a171542a9b5fe3c079c85277677663e9
Xaver <xdxaverdrabik>
2025-11-08 21:43:50 +0100

    save

---
+++ Frontend/src/components/Typography.jsx
@@ -0,0 +1,9 @@
+function TypographyH1({ children }) {
+    return (
+        <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+            {children} 12test
+        </h1>
+    )
+}
+
+export default TypographyH1
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TypographyH1 from "@/src/components/Typography.jsx";
+
+function Home() {
+
+    return (
+        <>
+            <TypographyH1>test</TypographyH1>
+        </>
+    )
+}
+
+export default Home
